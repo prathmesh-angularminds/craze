f060e2455b7a1f05fed29d009b71843ec1208c5a
prathmesh-angularminds <prathmesh.vibhute@angularminds.in>
2023-12-29 13:18:26 +0530

    Vertical side bar added

--- client/src/app/shared/components/vertical-sidebar/vertical-sidebar.component.ts
+++ client/src/app/shared/components/vertical-sidebar/vertical-sidebar.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AvatarComponent } from '../avatar/avatar.component';
 
 @Component({
   selector: 'app-vertical-sidebar',
   templateUrl: './vertical-sidebar.component.html',
   styleUrls: ['./vertical-sidebar.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule,AvatarComponent]
 })
 export class VerticalSidebarComponent implements OnInit {
 
@@ -17,18 +18,14 @@ export class VerticalSidebarComponent implements OnInit {
       title: "Navigation",
       content: [
         {
-          isDropdown: true,
           imageUrl: "../../../../assets/images/vertical-sidebar-images/cube.svg",
           title: "Products"
         },
         {
-          isDropdown: false,
           imageUrl: "../../../../assets/images/vertical-sidebar-images/truck.svg",
           title: "Orders"
         },
         {
-          isDropdown: false,
-
           imageUrl: "../../../../assets/images/vertical-sidebar-images/mail.svg",
           title: "Inbox"
         }
@@ -38,17 +35,14 @@ export class VerticalSidebarComponent implements OnInit {
       title: "Organization Settings",
       content: [
         {
-          isDropdown: false,
           imageUrl: "../../../../assets/images/vertical-sidebar-images/cube.svg",
           title: "Organization Details"
         },
         {
-          isDropdown: false,
           imageUrl: "../../../../assets/images/vertical-sidebar-images/users.svg",
           title: "Users"
         },
         {
-          isDropdown: false,
           imageUrl: "../../../../assets/images/vertical-sidebar-images/mail.svg",
           title: "Preferences"
         }
